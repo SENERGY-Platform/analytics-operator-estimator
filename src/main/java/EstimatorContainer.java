8f627ec94ec220413bf1123a79845127ed2c440c
franzmueller <franzmueller@users.noreply.github.com>
2019-10-17 11:50:58 +0200

    switch to AdaptiveRandomForestRegressor algorithm

--- src/main/java/EstimatorContainer.java
+++ src/main/java/EstimatorContainer.java
@@ -2,7 +2,7 @@ import com.yahoo.labs.samoa.instances.Attribute;
 import com.yahoo.labs.samoa.instances.Instances;
 import com.yahoo.labs.samoa.instances.InstancesHeader;
 import moa.classifiers.Classifier;
-import moa.classifiers.functions.AdaGrad;
+import moa.classifiers.meta.AdaptiveRandomForestRegressor;
 
 import java.util.List;
 
@@ -15,6 +15,7 @@ public class EstimatorContainer {
         Instances instances = new Instances(name, attributes, 0);
         instances.setClassIndex(attributes.size() - 1);
         header = new InstancesHeader(instances);
-        classifier = new AdaGrad();
+        classifier = new AdaptiveRandomForestRegressor();
+        classifier.prepareForUse();
     }
 }
